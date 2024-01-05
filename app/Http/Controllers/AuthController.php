b0a4fe8cfbee53540742c872e6bfc54ec1dd3760
Edson Junior <edson.junior@mereo.com>
2024-01-05 18:19:51 -0300

    Criando a rota login para obter o token JWT
    Essa controller retora um json com a mensagem ou o token válido.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,9 +6,46 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function login()
+    public function login(Request $request)
     {
-        return 'login';
+        /*Imprimir campo especifico dos dodos de entrada
+        Nesse caso estou verificando o email que foi enviado via post
+            *dd($request->all(['email']));*
+        */
+
+        
+        try {
+            /*
+            Estou realizando uma validação pelo email e senha.
+            O método attempt retorna um token quando a validação
+            é realizada com sucesso
+        */
+            $token = auth('api')->attempt(  
+    array(
+                    'email'    => $request->email,
+                    'password' => $request->password
+                )); 
+        } catch (\Throwable $th) {
+            return response()->json(['Message' => $th->getMessage()], $th->getCode());
+        }
+
+        if(!$token) {
+            /* 
+                Nesse exemplo utilizei o 403
+                Pois, o 401 está relacionado autorização
+                já o 403 está relacionado a autenticação.
+            */
+
+            return response()->json(['Message' => 'Usuário ou senha inválido.'], 403);
+        }
+
+        /* 
+            Retornando o response informando o token e o status da requisição.
+             *return response($token, 201);*
+
+            Nesse exemplo vou retornar um
+        */
+        return response()->json(['token' => $token], 201);
     }
 
     public function logout()
